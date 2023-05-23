463b9908e1086355b0381205f35ff461e0b3ab65
Rohan Ghadge <113715810+Rohan1366@users.noreply.github.com>
2023-05-23 10:59:35 +0530

    added project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='App'>
+        App
+      </div>
+       
+    </>
+  )
+}
+
+export default App
